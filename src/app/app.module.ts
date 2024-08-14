2a2f047d5fc259c27928b3565f326c7bfa95fd93
Ridmi1012 <ridmikaveeshasilva@gmail.com>
2024-08-14 11:40:39 +0530

    order inventory UI done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './auth/registration/registration.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './auth/login/login.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AddInventoryComponent } from './features/add-inventory/add-inventory.component';
 import { UpdateDeleteInventoryComponent } from './features/update-delete-inventory/update-delete-inventory.component';
 import { MatTableModule } from '@angular/material/table';
@@ -17,6 +15,13 @@ import { ReportsComponent } from './reports/reports.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarAdminComponent } from './components/navbar-admin/navbar-admin.component';
+import { OrderInventoryComponent } from './features/order-inventory/order-inventory.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -31,17 +36,23 @@ import { NavbarAdminComponent } from './components/navbar-admin/navbar-admin.com
     HeaderComponent,
     FooterComponent,
     ReportsComponent,
-    NavbarAdminComponent
+    NavbarAdminComponent,
+    OrderInventoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     MatTableModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
+
