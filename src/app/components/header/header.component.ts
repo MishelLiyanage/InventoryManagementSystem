f62d208caf0aaef0749f064d62e409217393212b
MishelLiyanage <mailtomishel@gmail.com>
2024-08-22 07:24:24 +0530

    getting the current user done and the logout function done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  firstname: string | null = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.firstname = this.authService.getfirstname();
+    console.log("Logged in user ID:", this.firstname);
+  }
+
+  logout() {
+    this.authService.clearUserdetails();
+    this.router.navigate(['/login']); // Redirect to the login page
+  }
 }
