1539e5638af883ef1893e094a37dd49aed75cd8d
miyulakshi <152995463+miyulakshi@users.noreply.github.com>
2024-08-22 22:50:45 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ViewInventoryComponent } from './features/view-inventory/view-inventory.component';
+import { AuthModule } from './auth/auth.module';
+import { FeatureModule } from './features/feature.module';
+import { DashboardModule } from './dashboards/dashboard.module'; 
+import { ReportModule } from './reports/report.module';
 
 const routes: Routes = [
-  { path: 'admin-view-inventory', component: ViewInventoryComponent },
-  { path: 'user-view-inventory', component: ViewInventoryComponent },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then((module) => module.AuthModule),
+  },
+  {
+    path: 'feature',
+    loadChildren: () =>
+      import('./features/feature.module').then((module) => module.FeatureModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboards/dashboard.module').then((module) => module.DashboardModule),
+  },
+  {
+    path: 'report',
+    loadChildren: () =>
+      import('./reports/report.module').then((module) => module.ReportModule),
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    AuthModule,
+    FeatureModule,
+    DashboardModule,
+    ReportModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
