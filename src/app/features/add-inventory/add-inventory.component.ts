64f09559d522f18e9c99501df6f1a0c1a1621f3c
MishelLiyanage <mailtomishel@gmail.com>
2024-08-23 00:53:33 +0530

    Current user got to the add inventory component

--- src/app/features/add-inventory/add-inventory.component.ts
+++ src/app/features/add-inventory/add-inventory.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { InventoryService } from '../../services/inventory.service';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-add-inventory',
@@ -8,11 +9,23 @@ import { NgForm } from '@angular/forms';
   styleUrl: './add-inventory.component.css'
 })
 export class AddInventoryComponent {
-  constructor(private inventoryService: InventoryService) { }
+  id: number | null = 0;
 
+  constructor(private inventoryService: InventoryService, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.id = this.authService.getUserId();
+    console.log("Logged in user ID:", this.id);
+  }
+
+  
   addItem(form: NgForm) {
+    if(this.id !== null){
+      const formData = {...form.value, adminid: this.id};
     if (form.valid) {
-      this.inventoryService.addItem(form.value).subscribe(
+      this.inventoryService.addItem(formData).subscribe(
         response => {
           console.log('Item added successfully', response);
           form.reset(); // Reset the form after submission
@@ -21,6 +34,7 @@ export class AddInventoryComponent {
           console.error('Error adding item', error);
         }
       );
+    }
     }
   }
 }
